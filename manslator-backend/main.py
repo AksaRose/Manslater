d9b76515c228d8df0e2c73c4ec18585ff0891f59
Aksa Rose <117498997+AksaRose@users.noreply.github.com>
2025-11-04 20:00:49 +0530

    fix: deployment cors issue

--- manslator-backend/main.py
+++ manslator-backend/main.py
@@ -97,7 +97,8 @@ app.add_middleware(
         "http://localhost:3000",
         "http://localhost:5173",
         "http://127.0.0.1:3000",
-        "http://127.0.0.1:5173"
+        "http://127.0.0.1:5173",
+        "https://www.manslater.in"
     ],
     allow_credentials=True,
     allow_methods=["*"],
